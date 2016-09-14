c53697dead13b2f089c70f09c682d9c9c3589272
Alex Lach <alexglach@gmail.com>
2016-09-14 14:18:52 -0400

    starting to build model

--- model.js
+++ model.js
@@ -1,4 +1,21 @@
 var TETRIS = TETRIS || {};
 
 // model
-//  gameboard = nested array
+//  gameboard = nested array that adds set pieces as they are set
+//  current_piece = array of coordinates [[[1,2][1,3][1,4]], orange]
+//  
+//  
+
+TETRIS.game = {
+
+  init: function() {
+    this.createBoard();
+  }
+
+  createBoard: function(){
+    var board = new Array(10);
+  }
+
+
+
+};
